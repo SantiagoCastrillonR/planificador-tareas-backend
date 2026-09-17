69e9ed217c2119571702488b79b39b30dfec9d1b
SantiagoCastrillonR <castrillonrojosantiago@gmail.com>
2026-09-17 14:41:34 -0500

    Actualizacion

---
+++ planflow/src/test/java/com/santiago/planflow/PlanflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santiago.planflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
